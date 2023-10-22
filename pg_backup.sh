2805f9d8e0f0668195ba2bd59df3607667f12e7e
p3rtang <p3rtang@Arch.debianP3>
2023-10-22 22:01:53 +0200

    fix: container name for backup

--- pg_backup.sh
+++ pg_backup.sh
@@ -13,7 +13,7 @@ do
 
     backup_num+=1
 
-    docker exec -t postgres_tallyWeb pg_dump -U p3rtang -d tally_web > "db-backup/dbdump_${file_name}"
+    docker exec -t postgres_tallyweb pg_dump -U p3rtang -d tally_web > "db-backup/dbdump_${file_name}"
     echo "backup done stored as dbdump_${file_name}"
     sleep 3600
 done
