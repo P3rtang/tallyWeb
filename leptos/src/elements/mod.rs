7fad560619c0c78167ef284696d2221a399aa85f
p3rtang <tomverhulst21@gmail.com>
2024-02-13 00:22:54 +0100

    fix: cargo fmt

--- leptos/src/elements/mod.rs
+++ leptos/src/elements/mod.rs
@@ -1,16 +1,16 @@
+mod about;
+mod account_icon;
 mod add_counter;
 mod context_menu;
 mod infobox;
 mod navbar;
 mod sort_search;
-mod about;
-mod account_icon;
 
 pub use super::{app::*, *};
+pub use about::AboutDialog;
+pub use account_icon::{letter_to_three_digit_hash, AccountIcon};
 pub use add_counter::*;
 pub use context_menu::*;
 pub use infobox::*;
 pub use navbar::*;
 pub use sort_search::*;
-pub use about::AboutDialog;
-pub use account_icon::{AccountIcon, letter_to_three_digit_hash};
