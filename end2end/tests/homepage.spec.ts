6e3f3e86db5d7645fd3a5209680e625250fa72cb
p3rtang <tomverhulst21@gmail.com>
2024-02-15 00:45:00 +0100

    test: more e2e testing

--- end2end/tests/homepage.spec.ts
+++ end2end/tests/homepage.spec.ts
@@ -7,36 +7,52 @@ test("homepage has title and links to intro page", async ({ page }) => {
 
 test("homepage has account icon and overlay", async ({ page }) => {
     await page.goto("http://localhost:3000/");
+    // make sure the wasm binary is loaded before clicking login
+    await page.waitForLoadState("networkidle");
+
     let accountIcon = page.getByTestId("test-account-icon")
     await expect(accountIcon).toBeVisible()
     await expect(accountIcon).toHaveText("U")
-
-    // make sure the wasm binary is loaded before clicking login
-    await page.waitForLoadState("networkidle");
 
     // test overlay on click
     await accountIcon.click()
     await expect(page.getByTestId("test-account-overlay")).toBeVisible()
 })
 
-test("sidebar", async ({ browser }) => {
-    let full_size_page = await browser.newContext({ viewport: { width: 1920, height: 1080 } }).then((ctx) => ctx.newPage());
-    let small_page = await browser.newContext({ viewport: { width: 1024, height: 576 } }).then((ctx) => ctx.newPage());
+test("sidebar style", async ({ browser }) => {
+    let pages = [
+        {
+            page: await browser.newContext({ viewport: { width: 1920, height: 1080 } }).then((ctx) => ctx.newPage()),
+            sidebar: { position: "relative" }
+        },
+        {
+            page: await browser.newContext({ viewport: { width: 1024, height: 576 } }).then((ctx) => ctx.newPage()),
+            sidebar: { position: "fixed" }
+        },
+    ];
 
-    {
-        await full_size_page.goto("http://localhost:3000");
+    for (const page of pages) {
+        await page.page.goto("http://localhost:3000")
         // make sure the wasm binary is loaded before clicking login
-        await full_size_page.waitForLoadState("networkidle");
+        await page.page.waitForLoadState("networkidle")
 
-        let sidebar = full_size_page.getByTestId("test-sidebar")
-        await expect(sidebar).toHaveCSS("position", "relative")
+        let sidebar = page.page.locator("side-bar")
+        await expect(sidebar).toBeVisible()
+        await expect(sidebar).toHaveCSS("position", page.sidebar.position)
     }
-    {
-        await small_page.goto("http://localhost:3000");
-        // make sure the wasm binary is loaded before clicking login
-        await small_page.waitForLoadState("networkidle");
+})
 
-        let sidebar = small_page.getByTestId("test-sidebar")
-        await expect(sidebar).toHaveCSS("position", "fixed")
-    }
+test("create counter", async ({ page }) => {
+    await page.goto("http://localhost:3000")
+    // make sure the wasm binary is loaded before clicking login
+    await page.waitForLoadState("networkidle")
+
+    let sidebar = page.locator("side-bar")
+    let newCounterButton = sidebar.getByRole("button", { name: "New Counter" })
+
+    await expect(newCounterButton).toBeVisible()
+    await newCounterButton.click()
+
+    let treeviewElement = sidebar.locator(".row span").locator("nth=0")
+    await expect(treeviewElement).toContainText('Counter 1')
 })
