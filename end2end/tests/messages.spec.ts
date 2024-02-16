6a3cd8b36b92151e98b014fa26b06c5149e2296d
p3rtang <tomverhulst21@gmail.com>
2024-02-16 10:44:38 +0100

    test: implemented testing for the message box

--- end2end/tests/messages.spec.ts
+++ end2end/tests/messages.spec.ts
@@ -6,6 +6,27 @@ test("show messages", async ({ page }) => {
     // make sure the wasm binary is loaded before clicking login
     await page.waitForLoadState("networkidle");
 
-    let message1 = page.getByText("message 1")
+    let notificationBox = page.locator("notification-box")
+    await expect(notificationBox).toBeVisible()
+
+    // this message has a timeout and will disappear with a fade
+    // therefore this should always run first after load
+    let timoutMessage = notificationBox.locator("dialog", { hasText: "message 4" })
+    await expect(timoutMessage).toHaveClass("fade-out")
+    await expect(timoutMessage).toBeHidden()
+
+    let message1 = notificationBox.locator("dialog", { hasText: "message 1" })
     await expect(message1).toBeVisible()
+
+    let closeMessage = message1.locator("button")
+    await expect(closeMessage).toBeHidden()
+    message1.hover()
+    await expect(closeMessage).toBeVisible()
+
+    await closeMessage.click()
+    await expect(message1).toHaveClass("fade-out")
+    await expect(message1).toBeHidden()
+
+    let errorMessage = notificationBox.locator("dialog", { hasText: "error" })
+    await expect(errorMessage).toHaveCSS("border", "2px solid rgb(255, 99, 71)")
 })
