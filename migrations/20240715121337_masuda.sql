8d23400c43856cf7e7142d0aaac0d2d985c8c823
p3rtang <tomverhulst21@gmail.com>
2024-07-25 00:48:06 +0200

    fix: migration error when in weird db state

--- migrations/20240715121337_masuda.sql
+++ migrations/20240715121337_masuda.sql
@@ -1,4 +1,4 @@
 -- Add migration script here
-ALTER TYPE hunttype  ADD VALUE 'MasudaGenIV';
-ALTER TYPE hunttype  ADD VALUE 'MasudaGenV';
-ALTER TYPE hunttype  ADD VALUE 'MasudaGenVI';
+ALTER TYPE hunttype ADD VALUE IF NOT EXISTS 'MasudaGenIV';
+ALTER TYPE hunttype ADD VALUE IF NOT EXISTS 'MasudaGenV';
+ALTER TYPE hunttype ADD VALUE IF NOT EXISTS 'MasudaGenVI';
