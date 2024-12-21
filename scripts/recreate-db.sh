865eee05d1ee3035ec4b472fb25620f343227887
p3rtang <tomverhulst21@gmail.com>
2024-12-21 22:57:03 +0100

    fix: environment issues

---
+++ scripts/recreate-db.sh
@@ -0,0 +1,14 @@
+docker compose up -d postgres
+psql -U postgres -d postgres -h localhost -p $POSTGRES_PORT -w -c " \
+    select pg_terminate_backend(pid) from pg_stat_activity where datname='$PGDATABASE'; \
+"
+psql -U postgres -d postgres -h localhost -p $POSTGRES_PORT -w -c "DROP DATABASE IF EXISTS $PGDATABASE"
+psql -U postgres -d postgres -h localhost -p $POSTGRES_PORT -w -c "CREATE DATABASE $PGDATABASE OWNER $POSTGRES_USERNAME"
+
+# setup sqlx migrations
+sqlx database create
+sqlx migrate run
+
+psql -U p3rtang -d $PGDATABASE -h localhost -p $POSTGRES_PORT -w -f ".github/postgres_setup/setup-test.sql"
+
+docker compose down
