61aacb254103137e08c48b81027949258c166be9
p3rtang <tomverhulst21@gmail.com>
2025-04-16 21:48:14 +0200

    fix: text overflow with new `Text` element

---
+++ frontend/src/elements/text/mod.rs
@@ -0,0 +1,19 @@
+use super::*;
+
+stylance::import_style!(style, "./text.module.scss");
+
+#[component]
+pub fn Text(
+    children: ChildrenFn,
+    #[prop(optional)] fade: bool,
+    #[prop(optional, into)] xstyle: Signal<XStyle>,
+) -> impl IntoView {
+    view! {
+        <span
+            class=stylance::classes!(style::text, fade.then_some(style::fade))
+            {..xstyle.get().into_any_attr()}
+        >
+            {children()}
+        </span>
+    }
+}
