8759d0fa4d778d3bcea2bd1d5c1510ed46a497a0
p3rtang <tomverhulst21@gmail.com>
2025-04-19 18:40:51 +0200

    feat: allow toggle for infobox parts

--- frontend/src/elements/button/button.rs
+++ frontend/src/elements/button/button.rs
@@ -51,20 +51,17 @@ pub fn Button(
         })
     } else {
         Either::Right(view! {
-            <AttributeInterceptor let:attrs>
-                <button
-                    node_ref=node_ref
-                    class=stylance::classes!(
-                        style::button,
-                        hover.into_class(),
-                        class.get().as_str()
-                    )
-                    {..xstyle.border_radius()}
-                    {..attrs}
-                >
-                    <div {..xstyle.into_any_attr()}>{children.get_value()()}</div>
-                </button>
-            </AttributeInterceptor>
+            <button
+                node_ref=node_ref
+                class=stylance::classes!(
+                    style::button,
+                    hover.into_class(),
+                    class.get().as_str()
+                )
+                {..xstyle.border_radius()}
+            >
+                <div {..xstyle.into_any_attr()}>{children.get_value()()}</div>
+            </button>
         })
     }
 }
